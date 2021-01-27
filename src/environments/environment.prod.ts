f5b3deb1910461d9321b875bb48eaa97e3cab480
PillouMatou <vincent.armone@etu.univ-grenoble-alpes.fr>
2021-01-27 15:45:39 +0100

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+  apiKey: "AIzaSyAZlYWiHQxm3b0GXds3pxIMxrcbm_czJ9I",
+  authDomain: "ionic-m2gi.firebaseapp.com",
+  projectId: "ionic-m2gi",
+  storageBucket: "ionic-m2gi.appspot.com",
+  messagingSenderId: "37327013678",
+  appId: "1:37327013678:web:bb94bf24ea6db208aa1cbf",
+  measurementId: "G-49SBL3HRQ6"
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAZlYWiHQxm3b0GXds3pxIMxrcbm_czJ9I",
+    authDomain: "ionic-m2gi.firebaseapp.com",
+    projectId: "ionic-m2gi",
+    storageBucket: "ionic-m2gi.appspot.com",
+    messagingSenderId: "37327013678",
+    appId: "1:37327013678:web:bb94bf24ea6db208aa1cbf",
+    measurementId: "G-49SBL3HRQ6"
+  }
 };
 
 /*
